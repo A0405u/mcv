4fa408bff30cf725cc2a73a3627aba287f5ea825
A0405u <A0405u@ya.ru>
2020-12-15 20:23:16 +0300

    commit

--- test1.py
+++ test1.py
@@ -7,41 +7,6 @@ CAPSIZEY = 720
 DSPSIZEX = 480
 DSPSIZEY = 320
 
-from PIL import Image
-from PIL import ImageEnhance
-
-
-def threshold(frame, step=20):
-
-    temp = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    img = Image.fromarray(temp, "RGB").convert("L")
-    img = ImageEnhance.Contrast(img).enhance(1.2)
-    pixels = list(img.getdata())
-    arr    = np.array(pixels)
-    arr2d  = arr.reshape(img.size)
-
-    blocks = np.reshape(arr2d, (-1, step, step))
-
-    for block in blocks:
-        factor = 1.2 # CHANGE DEPENDING ON RESULT -> Need algorithm
-        mean   = np.mean(block)
-        thresh = mean/factor
-
-        block[block <= thresh] = 0
-        block[block > thresh] = 1
-
-    return np.reshape(blocks, (1, -1))
-
-    # img2  = Image.new("1", img.size)
-    # img2.putdata(arr2d[0].tolist())
-    # img2.save("test.jpg")
-    # img2.show()
-
-
-
-    return img2
-
-
 
 def gstreamer_pipeline(
     capture_width = CAPSIZEX,
@@ -83,7 +48,9 @@ def show_camera():
 
         ret_val, frame = cap.read()
 
-        binary = threshold(frame)
+        #binary = threshold(frame)
+
+        binary = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
         # Show video
         cv2.imshow('Original', frame)
