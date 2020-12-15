b45c10969a67bc2f34f5adfdf31aa6bb06c10977
A0405u <A0405u@ya.ru>
2020-12-15 17:16:37 +0300

    commit

--- test1.py
+++ test1.py
@@ -5,32 +5,32 @@ from PIL import Image
 from PIL import ImageEnhance
 
 
-def threshold(filename, step=20):
-    img    = Image.open(filename).convert("L")
-    img = ImageEnhance.Contrast(img).enhance(1.2)
-    pixels = list(img.getdata())
-    arr    = np.array(pixels)
-    arr2d  = arr.reshape(img.size)
+# def threshold(filename, step=20):
+#     img    = Image.open(filename).convert("L")
+#     img = ImageEnhance.Contrast(img).enhance(1.2)
+#     pixels = list(img.getdata())
+#     arr    = np.array(pixels)
+#     arr2d  = arr.reshape(img.size)
 
-    blocks = np.reshape(arr2d, (-1, step, step))
-    for block in blocks:
-        factor = 1.2 # CHANGE DEPENDING ON RESULT -> Need algorithm
-        mean   = np.mean(block)
-        thresh = mean/factor
+#     blocks = np.reshape(arr2d, (-1, step, step))
+#     for block in blocks:
+#         factor = 1.2 # CHANGE DEPENDING ON RESULT -> Need algorithm
+#         mean   = np.mean(block)
+#         thresh = mean/factor
 
-        block[block <= thresh] = 0
-        block[block > thresh] = 1
+#         block[block <= thresh] = 0
+#         block[block > thresh] = 1
 
-    arr2d = np.reshape(blocks, (1, -1))
+#     arr2d = np.reshape(blocks, (1, -1))
 
-    img2  = Image.new("1", img.size)
-    img2.putdata(arr2d[0].tolist())
-    img2.save("test.jpg")
-    img2.show()
+#     img2  = Image.new("1", img.size)
+#     img2.putdata(arr2d[0].tolist())
+#     img2.save("test.jpg")
+#     img2.show()
 
 
 
-    return img2
+#     return img2
 
 
 
