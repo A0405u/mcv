759ae714ac83f61a69b5185009d43dc3b10be760
A0405u <A0405u@ya.ru>
2020-12-15 18:48:51 +0300

    commit

--- test1.py
+++ test1.py
@@ -7,24 +7,18 @@ CAPSIZEY = 720
 DSPSIZEX = 480
 DSPSIZEY = 320
 
-# from PIL import Image
-# from PIL import ImageEnhance
+from PIL import Image
+from PIL import ImageEnhance
 
 
-def threshold(img, step=20):
+def threshold(frame, step=20):
 
-    # img    = Image.open(filename).convert("L")
-    # img = ImageEnhance.Contrast(img).enhance(1.2)
-    # pixels = list(img.getdata())
-    # arr    = np.array(pixels)
-
-    arr2d = img.copy()
-
-    for line in arr2d:
-        for pixel in line:
-            pixel = list(pixel)
-
-    # arr2d = arr.reshape((DSPSIZEX, DSPSIZEY))
+    img = Image.fromarray(frame, "BGR;32")
+    img = img.convert("L")
+    img = ImageEnhance.Contrast(img).enhance(1.2)
+    pixels = list(img.getdata())
+    arr    = np.array(pixels)
+    arr2d  = arr.reshape(img.size)
 
     blocks = np.reshape(arr2d, (-1, step, step))
 
@@ -36,11 +30,16 @@ def threshold(img, step=20):
         block[block <= thresh] = 0
         block[block > thresh] = 1
 
-    arr2d = np.reshape(blocks, (1, -1))
+    return np.reshape(blocks, (1, -1))
 
-    # for line in arr2d:
-    #     for pixel in line:
-    #         pixel = np.array(pixel)
+    # img2  = Image.new("1", img.size)
+    # img2.putdata(arr2d[0].tolist())
+    # img2.save("test.jpg")
+    # img2.show()
+
+
+
+    return img2
 
 
 
