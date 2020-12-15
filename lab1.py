7fc83a0ce01d3b805e0a113575c010d544bc1235
A0405u <A0405u@ya.ru>
2020-12-15 16:32:12 +0300

    commit

--- lab1.py
+++ lab1.py
@@ -15,8 +15,8 @@ while(cap.isOpened()):
 
     invert = ~frame
 
-    frame = cv2.resize(frame, None, None, fx=0.5, fy=0.5)
-    invert = cv2.resize(invert, None, None, fx=0.5, fy=0.5)
+    frame = cv2.resize(frame, None, None, fx=2, fy=2)
+    invert = cv2.resize(invert, None, None, fx=2, fy=2)
 
     # Show video
     cv2.imshow('Cam', frame)
