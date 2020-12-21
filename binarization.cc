c0aa344657de532c40e6186b894960b1c48a887e
A0405u <A0405u@ya.ru>
2020-12-21 20:49:42 +0300

    commit

--- binarization.cc
+++ binarization.cc
@@ -2,7 +2,7 @@
 
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include <opencv2/imgproc.hpp>
+//#include <opencv2/imgproc.hpp>
 
 #include <iostream>
 #include <arm_neon.h>
